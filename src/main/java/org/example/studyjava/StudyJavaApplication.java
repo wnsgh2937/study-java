aed12215967f489152410ae0bcb6673507655559
kakaoent-leo <leo.ee@kakaoent.com>
2024-03-20 19:42:35 +0900

    init

---
+++ src/main/java/org/example/studyjava/StudyJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJavaApplication.class, args);
+    }
+
+}
